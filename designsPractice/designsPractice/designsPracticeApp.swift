fa2475f765a32b55ebec743159a795e89c1cfec5
Markel <markjuaristi@gmail.com>
2023-07-10 13:03:42 +0200

    add xcode project

---
+++ designsPractice/designsPractice/designsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  designsPracticeApp.swift
+//  designsPractice
+//
+//  Created by Markel Juaristi on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct designsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
