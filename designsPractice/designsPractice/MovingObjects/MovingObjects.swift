e4d3717cf4e25e8312d03343e9e0208ca36e7e47
Markel <markjuaristi@gmail.com>
2023-07-12 17:57:14 +0200

    prueba de diferentes formas el moving objetcs, pero no lo consigo con la version 2, seguir probando

--- designsPractice/designsPractice/MovingObjects/MovingObjects.swift
+++ designsPractice/designsPractice/MovingObjects/MovingObjects.swift
@@ -10,6 +10,42 @@ import SwiftUI
  En este ejemplo, tenemos tres círculos que se mueven arriba y abajo en la pantalla. Usamos offset para mover los círculos, y repeatForever(autoreverses: true) para hacer que la animación se repita indefinidamente y se revierta después de cada ciclo. La animación comienza automáticamente cuando la vista aparece gracias a onAppear.
  */
 
+//version 2
+import SwiftUI
+
+struct MovingObjects: View {
+    @State private var isAnimating = false
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            HStack {
+                ForEach(0..<3) { _ in
+                    Circle()
+                        .frame(width: 60, height: 60)
+                        .foregroundColor(.blue)
+                        .offset(x: isAnimating ? 0 : -100, y: 0)
+                }
+            }
+            .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: true))
+            .onAppear {
+                isAnimating = true
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct MovingObjects_Previews: PreviewProvider {
+    static var previews: some View {
+        MovingObjects()
+    }
+}
+
+//version 1
+/*
 struct MovingObjects: View {
     @State private var isAnimating = false
     
@@ -41,3 +77,4 @@ struct MovingObjects_Previews: PreviewProvider {
     }
 }
 
+*/
